c488a667b9863b9a38d64c4272f0f503ee65bf1e
Bruno Oliveira <bruno@faber.vc>
2016-04-22 15:49:00 +0100

    Initial commit

---
+++ app/src/main/kotlin/com/boliveira/rxkotlin/network/CrunchBaseService.kt
@@ -0,0 +1,48 @@
+package com.boliveira.rxkotlin.network
+
+import com.boliveira.rxkotlin.network.CrunchBaseApi
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.logging.HttpLoggingInterceptor
+import retrofit.GsonConverterFactory
+import retrofit.Retrofit
+import retrofit.RxJavaCallAdapterFactory
+
+
+/**
+ * Created by bruno on 18/04/16.
+ */
+
+class CrunchBaseService {
+    companion object {
+        val baseEndpoint = "https://api.crunchbase.com/v/3/"
+
+        private var _service: CrunchBaseApi? = null
+
+        val builder: CrunchBaseApi
+            get() {
+                _service?.let {
+                    return it
+                }
+
+                var logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BASIC
+
+                var client = OkHttpClient()
+                client.interceptors().add(logging)
+
+                var retrofit = Retrofit.Builder()
+                        .baseUrl(baseEndpoint)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                        .client(client)
+                        .build()
+
+                _service = retrofit.create(CrunchBaseApi::class.java)
+                return _service!!
+            }
+    }
+}
+
+
+
+
---
+++ app/src/main/kotlin/com/boliveira/rxkotlin/rxutil/Variable.kt
@@ -0,0 +1,16 @@
+package com.boliveira.rxkotlin.rxutil
+
+import rx.lang.kotlin.BehaviorSubject
+
+class Variable<T>(value: T) {
+    private var _valuePublisher = BehaviorSubject<T>()
+
+    var value: T = value
+    set(newValue) {
+        synchronized(this) {
+            _valuePublisher.onNext(newValue)
+        }
+    }
+
+    fun asObservable() = _valuePublisher.asObservable()
+}
